3482abd0030edbd6487e4b09d9e7bd7e69bbcaea
imin02 <46943452+imin02@users.noreply.github.com>
2020-05-20 14:17:32 +0800

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "Imin" },
+    { quote: "I love you.", author: "IMin" },
+    { quote: "Aku sayang kamu!", author: " min" },
+    { quote: "bedebes ♥", author: " IMin!" },
+    { quote: "Love chuuu", author: "bct" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["des", "debedes", "Sayang", "Babe", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
